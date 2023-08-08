92568066a981297f38769d4b7e3c7ff1317415a7
abhishekcse08 <abhishekcse08@gmail.com>
2023-08-08 08:23:48 +0530

    pom changes

--- src/main/java/com/magnus/crud/example/repository/ProductRepository.java
+++ src/main/java/com/magnus/crud/example/repository/ProductRepository.java
@@ -2,8 +2,11 @@ package com.magnus.crud.example.repository;
 
 import com.magnus.crud.example.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+
+
     Product findByName(String name);
 }
 
